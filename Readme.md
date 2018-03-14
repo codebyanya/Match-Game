7f43a1ed28c337813025ef43c7cc939c6b88c247
codebyanya <sembyanya@gmail.com>
2018-03-14 14:20:14 +0100

    Changes to be commited: First commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+Match Game
+Card-matching game built using Bootstrap, JavaScript, and jQuery.
+
+What is the Match Game
+Goal of the game is to test the player's memory. In a deck of paired cards, the player needs to match each pair to win the game.
---
+++ project/resources/js/match-game.js
@@ -0,0 +1,43 @@
+var MatchGame = {};
+
+/*
+  Sets up a new game after HTML document has loaded.
+  Renders a 4x4 board of cards.
+*/
+
+/*
+  Generates and returns an array of matching card values.
+ */
+
+MatchGame.generateCardValues = function () {
+
+var InitialCardValues =[];
+for (var i = 0; i <= 8; i++) {
+  InitialCardValues.push(i);
+  InitialCardValues.push(i);
+}
+};
+
+var cardValues = [];
+
+while (InitialCardValues.length > 0 ) {
+var randomIndex = [Math.floor(Math.random() * InitialCardValues.length)];
+}
+
+/*
+  Converts card values to jQuery card objects and adds them to the supplied game
+  object.
+*/
+
+MatchGame.renderCards = function(cardValues, $game) {
+
+};
+
+/*
+  Flips over a given card and checks to see if two cards are flipped over.
+  Updates styles on flipped cards depending whether they are a match or not.
+ */
+
+MatchGame.flipCard = function($card, $game) {
+
+};
