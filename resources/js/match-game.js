0c1293fe531d1e7d2dc30c6007d54cbceb7643a3
codebyanya <sembyanya@gmail.com>
2018-08-28 00:32:07 +0200

    adding clickCard function

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -37,3 +37,6 @@ const init = function () {
 
 /* Flips over a given card and checks to see if two cards are flipped over.
 Updates styles on flipped cards depending whether they are a match or not.*/
+
+const clickCard = function () {
+}
